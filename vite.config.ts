baecae0bedf2ae04bf923064b993b7ee3e9157f4
yaoshengjian <yaoshengjian@qq.com>
2024-01-10 12:44:03 +0800

    build: buffer

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,19 @@ import { defineConfig } from "vite";
 import { join } from "path";
 import react from "@vitejs/plugin-react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
+import inject from '@rollup/plugin-inject'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import rollupNodePolyFill from 'rollup-plugin-polyfill-node'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
   // plugins: [react()],
   build: {
     outDir: "./docs",
+    rollupOptions: {
+			// plugins: [inject({ Buffer: ['buffer', 'Buffer'] })],
+			plugins: [rollupNodePolyFill()]
+		},
   },
   base: "./",
   resolve: {
